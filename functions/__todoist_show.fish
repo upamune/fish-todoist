cedcc135f24363488c9fa7553340dbdcf747a147
Yu SERIZAWA (@upamune) <github@serizawa.me>
2018-01-06 13:59:42 +0900

    Add preview to show

--- functions/__todoist_show.fish
+++ functions/__todoist_show.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function __todoist_show
-  command todoist list | fzf --reverse --header='ShowTask' +m | cut -d ' ' -f 1 | tr '\n' ' ' | read -l tasks
+  command todoist list | fzf --reverse --header='ShowTask' --preview='command todoist show {1}' +m | cut -d ' ' -f 1 | tr '\n' ' ' | read -l tasks
   
   set cmd "todoist show "
   if not test -z $tasks
